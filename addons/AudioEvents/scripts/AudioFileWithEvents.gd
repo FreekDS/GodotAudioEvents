3dece50f631cd17616946a1d86c6218ea018edca
Freek <freek.de.sagher21@gmail.com>
2023-12-16 13:25:28 +0100

    Short documentation

--- addons/AudioEvents/scripts/AudioFileWithEvents.gd
+++ addons/AudioEvents/scripts/AudioFileWithEvents.gd
@@ -1,6 +1,9 @@
 @icon("res://addons/AudioEventPlayer.svg")
 class_name AudioEventsStreamResource
 extends Resource
+## Custom resource that manages an audio file together with an array of event times
+##
+## Use this Resource together with an AudioEventPlayer.
 
 @export var name : String = "Audio"
 @export var audio : AudioStream
